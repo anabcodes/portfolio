476346bf0d84e63c7637985cd548d6c4971cb10c
Ana Braghim <anajulimabr@gmail.com>
2025-10-18 13:34:05 -0300

    styles: borda na imagem da HeroSection adicionada

--- src/pages/Home/HeroSection.tsx
+++ src/pages/Home/HeroSection.tsx
@@ -7,7 +7,7 @@ function HeroSection(){
         <ContentSection>
             <section className="flex flex-col justify-center items-center md:gap-3 gap-5">
                 <h1 className="text-5xl md:text-6xl lg:text-8xl xl:text-9xl font-bold text-center">ANA BRAGHIM</h1>
-                <img className="w-60 h-60 rounded-full bg-neutral-200 object-cover" src="assets/images/ana-braghim.jpg" alt="" />
+                <img className="w-60 h-60 rounded-full bg-neutral-200 object-cover border-[1px]" src="assets/images/ana-braghim.jpg" alt="" />
                 <TextoDestaque texto="Front-End & Back-End • UI/UX • Cloud Computing"/>
                 <p className="text-center">Crio softwares que unem experiência do usuário, qualidade e boas práticas de engenharia.</p>
             </section>
